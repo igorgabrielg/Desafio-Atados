2827d161b194b8f68928f160c8e641791375d0a9
Igor Gabriel <igor.gabrielg@gmail.com>
2020-02-14 18:23:06 -0300

    Teste de gerar_valores desenvolvido

---
+++ test_operacoes.py
@@ -0,0 +1,14 @@
+﻿from operacoes import gerar_valores, melhor_situação
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_gerar_valores(self):
+        valores = (10, 7, 3)
+        for valor in valores:
+            self.assertEqual(len(gerar_valores(valor)), valor)
+            self.assertIs(type(gerar_valores(valor)), list)
+
+
+if __name__ == '__main__':
+    unittest.main()
