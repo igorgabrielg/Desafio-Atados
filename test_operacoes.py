61a9130608a3a92e986a76f3c208d554bf473fd5
Igor Gabriel <igor.gabrielg@gmail.com>
2020-02-14 18:39:57 -0300

    Teste Unitario da função de melhor_situacao() desenvolvido

--- test_operacoes.py
+++ test_operacoes.py
@@ -9,6 +9,17 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(len(gerar_valores(valor)), valor)
             self.assertIs(type(gerar_valores(valor)), list)
 
+    def test_melhor_situação(self):
+        testes = (
+            ([7, 1, 5, 3, 6, 4], 5),
+            ([7, 6, 4, 3, 1], 0),
+            ([1, 1, 1, 1], 0)
+        )
+
+        for teste, expectativa in testes:
+            self.assertEqual(melhor_situação(teste), expectativa)
+            self.assertIs(type(melhor_situação(teste)), int)
+
 
 if __name__ == '__main__':
     unittest.main()
